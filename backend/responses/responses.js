0bf44b501ec28b3a9697c23ee86735690d01116c
Anton Knutsson <cykelkorg95@gmail.com>
2024-11-13 09:28:49 +0100

    added responses for success and failed database responses

---
+++ backend/responses/responses.js
@@ -0,0 +1,22 @@
+export function sendResponse(status, data) {
+    return {
+        statusCode: status,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ success: true, data }),
+    };
+}
+
+export function sendError(status, message) {
+    return {
+        statusCode: status,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ success: false, error: message }),
+    };
+}
+
+
+// Författare Anton
