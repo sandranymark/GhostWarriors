b14fb2a4f1c5579d056f866340f39f3e7362e554
AdreanRodriguez <loco_bladare@hotmail.com>
2024-11-14 14:46:56 +0100

    only hamburger left

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -7,15 +7,20 @@ import DforBreakfast from "../../assets/DforBreakfast.svg";
 function Header() {
   return (
     <header className="header">
-      <Link to={"/"}>
-        <img src={DforBreakfast} alt="D For Breakfast logo" />
+      <Link className="header__link" to={"/"}>
+        <img className="header__logo" src={DforBreakfast} alt="D For Breakfast logo" />
       </Link>
       <Nav />
       <div className="header__cart-btn--wrapper">
         <button className="header__btn">Login</button>
         <img className="header__cart" src={cart} alt="cart-logo" />
-        <p className="header__cart-items">4</p>
+        <p className="header__cart-items"></p>
       </div>
+      <nav className="hamburger">
+        <hr className="hamburger-line" />
+        <hr className="hamburger-line" />
+        <hr className="hamburger-line" />
+      </nav>
     </header>
   );
 }
