a6bc44d31b63f86089b3956879d7194598fa842a
Anton Knutsson <cykelkorg95@gmail.com>
2024-11-18 11:46:40 +0100

    increment buttons and addbtn done

---
+++ frontend/src/components/incrementButton/IncrementButton.tsx
@@ -0,0 +1,30 @@
+import './IncrementButton.css'
+import { useState } from 'react'
+
+function IncrementButton() {
+
+    const [value, setValue] = useState(0);
+
+    const increaseValue = () => {
+      setValue(value + 1);
+    };
+  
+    const decreaseValue = () => {
+      if(value > 0) {
+        setValue(value - 1);
+      }
+    };
+
+  return (
+    <div className='increment-section'>
+        <div className="menuItem-addToCart--btn">
+        <button className="decreace cart-btn" onClick={decreaseValue}>-</button>
+        <div className="display-value">{value}</div>
+        <button className="increase cart-btn" onClick={increaseValue}>+</button>
+      </div>
+      <button className="menuItem-addTocart">add to cart</button>
+    </div>
+  )
+}
+
+export default IncrementButton
--- frontend/src/pages/menupage/Menupage.tsx
+++ frontend/src/pages/menupage/Menupage.tsx
@@ -17,31 +17,37 @@ function Menupage() {
         <MenuItem
           image={ricePorridge}
           heading="Rice porridge"
+          price={50}
           description="A warm and creamy porridge made from rice, perfect for a comforting breakfast."
         />
         <MenuItem
           image={cake}
           heading="Delicious cake"
+          price={50}
           description="A sweet and moist cake, topped with a delightful glaze or cream."
         />
         <MenuItem
           image={baguette}
           heading="Baguette"
+          price={50}
           description="A classic French baguette, crispy on the outside and soft on the inside."
         />
         <MenuItem
           image={sandwich}
           heading="Sandwich"
+          price={50}
           description="A delicious sandwich filled with fresh ingredients and flavorful spreads."
         />
         <MenuItem
           image={avocadoToast}
           heading="Avocado toast"
+          price={50}
           description="Crispy toast topped with creamy avocado and a hint of lime."
         />
         <MenuItem
           image={breakfastPlate}
           heading="Breakfast plate"
+          price={50}
           description="A hearty breakfast plate featuring a variety of classic morning favorites."
         />
       </menu>
