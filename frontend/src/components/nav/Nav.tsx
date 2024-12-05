6aa52247c211d40eb64fc5bfe2e30eb7805f79df
AdreanRodriguez <loco_bladare@hotmail.com>
2024-12-05 12:15:56 +0100

    routing till staff är fixad

--- frontend/src/components/nav/Nav.tsx
+++ frontend/src/components/nav/Nav.tsx
@@ -1,5 +1,6 @@
 import "./Nav.css";
 import { Link } from "react-router-dom";
+import useAuthStore from "../../stores/authStore";
 
 interface NavProps {
   className?: string;
@@ -7,12 +8,15 @@ interface NavProps {
 }
 
 function Nav({ className = "", onClose }: NavProps) {
+  const user = useAuthStore((state) => state.user); // Hämta användarinformation från authStore
+
   function removeScrollOnBody(): void {
     const bodyRef = document.querySelector("body") as HTMLBodyElement;
     if (bodyRef.classList.contains("no-scroll")) {
       bodyRef.classList.remove("no-scroll");
     }
   }
+
   function handleLinkClick(): void {
     if (onClose) {
       onClose();
@@ -22,32 +26,47 @@ function Nav({ className = "", onClose }: NavProps) {
 
   return (
     <nav className={`nav ${className}`}>
-      <ul className="nav__ul">
-        <li className="nav__li">
-          <Link className="nav__link" to={"/"} onClick={handleLinkClick}>
-            Home
-          </Link>
-        </li>
-        <li className="nav__li">
-          <Link className="nav__link" to={"/about"} onClick={handleLinkClick}>
-            About
-          </Link>
-        </li>
-        <li className="nav__li">
-          <Link className="nav__link" to={"/contact"} onClick={handleLinkClick}>
-            Contact
-          </Link>
-        </li>
-        <li className="nav__li">
-          <Link className="nav__link" to={"/menu"} onClick={handleLinkClick}>
-            Menu
-          </Link>
-        </li>
+      <ul className="nav__ul staff-nav">
+        {user?.role === "admin" ? (
+          <>
+            <li className="nav__li">
+              <Link className="nav__link" to="/staff/menu" onClick={handleLinkClick}>
+                Menu
+              </Link>
+            </li>
+            <li className="nav__li">
+              <Link className="nav__link" to="/staff" onClick={handleLinkClick}>
+                Order
+              </Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li className="nav__li">
+              <Link className="nav__link" to={"/"} onClick={handleLinkClick}>
+                Home
+              </Link>
+            </li>
+            <li className="nav__li">
+              <Link className="nav__link" to={"/about"} onClick={handleLinkClick}>
+                About
+              </Link>
+            </li>
+            <li className="nav__li">
+              <Link className="nav__link" to={"/contact"} onClick={handleLinkClick}>
+                Contact
+              </Link>
+            </li>
+            <li className="nav__li">
+              <Link className="nav__link" to={"/menu"} onClick={handleLinkClick}>
+                Menu
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
 }
 
 export default Nav;
-
-// Författare: Adréan
